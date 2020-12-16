175b382f1b31a1d32ca7b6f236edbff545c2743f
HorizonAks <akshat.jain634@gmail.com>
2020-12-16 19:03:09 +0530

    Updated Design

--- app/src/main/java/com/example/myfirstapp/searchResActivity.java
+++ app/src/main/java/com/example/myfirstapp/searchResActivity.java
@@ -8,14 +8,13 @@ import android.widget.TextView;
 
 public class searchResActivity extends AppCompatActivity {
 
-    String query = getIntent().getStringExtra("query");
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_res);
         TextView priceAmazon = (TextView) findViewById(R.id.textView2);
+        String query = getIntent().getStringExtra("query");
         priceAmazon.setText(query);
     }
 
