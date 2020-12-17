be8fc07199271e5771b71458c8d166b03ab5c793
HorizonAks <akshat.jain634@gmail.com>
2020-12-17 18:22:26 +0530

    Implemented Basic App Functionality

--- app/src/main/java/com/example/myfirstapp/searchResActivity.java
+++ app/src/main/java/com/example/myfirstapp/searchResActivity.java
@@ -3,8 +3,12 @@ package com.example.myfirstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class searchResActivity extends AppCompatActivity {
 
@@ -13,9 +17,51 @@ public class searchResActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_res);
-        TextView priceAmazon = (TextView) findViewById(R.id.textView2);
-        String query = getIntent().getStringExtra("query");
-        priceAmazon.setText(query);
+        initialize_views();
+        AmazonApiCall();
+        FlipkartApiCall();
+
+
+    }
+
+    public String getProduct(){
+        //gets product entered from mainActivity
+        final String product = getIntent().getStringExtra("query");
+        if (product != null){
+            return product;
+        }else{
+            return "Not Found";
+        }
+    }
+
+    public void initialize_views(){
+        //price
+        TextView Amazon = (TextView) findViewById(R.id.textView3);
+        TextView Flipkart = (TextView) findViewById(R.id.textView4);
+        Amazon.setText("Product Not Found");
+        Flipkart.setText("Product Not Found");
+
+        //initialize product searched
+        TextView product = (TextView) findViewById(R.id.textView2);
+        product.setText(getProduct());
+
+        //links
+        ImageView Amazonln = (ImageView) findViewById(R.id.imageView4);
+        ImageView Flipkartln = (ImageView) findViewById(R.id.imageView6);
+    }
+
+    public void AmazonApiCall(){
+        //TODO - setAmazonApi functionality
+        class Amazonprice extends siteprice{
+
+        }
+    }
+
+    public void FlipkartApiCall(){
+        //TODO - setFlipkartApi functionality
+        class Flipkartprice extends siteprice{
+
+        }
     }
 
 
---
+++ app/src/main/java/com/example/myfirstapp/siteprice.java
@@ -0,0 +1,6 @@
+package com.example.myfirstapp;
+
+public class siteprice {
+    private float price = 0;
+    private String url = "Error 404!";
+}
